e35dfa205627185e6461cd01be23b2e28a80992e
VixiXia <victoriaxia@outlook.com>
2024-01-29 15:11:43 +0800

    Delete dist/src directory

--- dist/src/env.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TempFileDir = void 0;
-exports.TempFileDir = process.env.NODE_ENV !== "local" ? "/tmp" : "./tmp";
-//# sourceMappingURL=env.js.map
